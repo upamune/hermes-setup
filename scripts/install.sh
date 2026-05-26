7c2fa8db7b7aa3b356f60b3f7b96ebac84a1f321
Yu SERIZAWA(@upamune) <info@serizawa.me>
2026-05-26 23:40:57 +0900

    Run setup user bootstrap from home

--- scripts/install.sh
+++ scripts/install.sh
@@ -88,7 +88,7 @@ if [ "$(id -u)" = "0" ] && [ -n "$setup_user" ] && [ -z "${HERMES_SETUP_BOOTSTRA
 
   export HERMES_SETUP_BOOTSTRAPPED_USER=1
   exec sudo --preserve-env=HERMES_SETUP_BOOTSTRAPPED_USER,HERMES_SETUP_REPO,HERMES_SETUP_REF,HERMES_SETUP_SKIP_SUMMON,HERMES_SETUP_SKIP_TAILSCALE,HERMES_SETUP_SKIP_HERMES_INSTALL,HERMES_SETUP_SKIP_SELF_INSTALL,HERMES_SETUP_NO_PROMPT,HERMES_SETUP_ALLOW_MISSING_BWS_TOKEN,HERMES_SETUP_NO_GATEWAY_START,HERMES_SETUP_LOCK_DOWN_INCOMING,HERMES_SETUP_FORCE_LOCK_DOWN_INCOMING,HERMES_SETUP_NO_DISABLE_SSHD,HERMES_SETUP_FORCE_DISABLE_SSHD,HERMES_BWS_PROJECT_ID,BWS_PROJECT_ID,BWS_ACCESS_TOKEN,TS_AUTHKEY,TS_HOSTNAME,HERMES_SETUP_TAILSCALE_HOSTNAME,TS_EXTRA_ARGS \
-    -H -u "$setup_user" sh -c 'curl -fsSL https://raw.githubusercontent.com/upamune/hermes-setup/main/scripts/install.sh | sh -s -- "$@"' hermes-setup "$@"
+    -H -u "$setup_user" sh -c 'cd && curl -fsSL https://raw.githubusercontent.com/upamune/hermes-setup/main/scripts/install.sh | sh -s -- "$@"' hermes-setup "$@"
 fi
 
 if [ "${HERMES_SETUP_SKIP_SUMMON:-0}" != "1" ]; then
